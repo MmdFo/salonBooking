5a28b67e6bfd464a23bf35015cec44c44441aa7d
mmd <mohammad.forghani.y83@gmail.com>
2026-07-28 19:11:14 +0330

    PortfolioItem created.  DB bug fixed

---
+++ src/main/java/com/mmdfo/salonbooking/entity/PortfolioItem.java
@@ -0,0 +1,46 @@
+package com.mmdfo.salonbooking.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PortfolioItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "salon_service_id", nullable = false)
+    private SalonService salonService;
+
+    @Column(nullable = false, length = 150)
+    private String imageUrl;
+
+    private LocalDateTime createdDate;
+
+
+}
