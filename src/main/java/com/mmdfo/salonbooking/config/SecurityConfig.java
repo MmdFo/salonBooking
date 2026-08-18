707f0fa025cafc34dd60a848c12b70807d4d6b03
mmd <mohammad.forghani.y83@gmail.com>
2026-08-18 16:30:39 +0330

    CORS access added to localhost:5500

--- src/main/java/com/mmdfo/salonbooking/config/SecurityConfig.java
+++ src/main/java/com/mmdfo/salonbooking/config/SecurityConfig.java
@@ -79,7 +79,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://127.0.0.1:5500")
+                List.of("http://127.0.0.1:5500"
+                        ,"http://localhost:5500")
         );
 
         configuration.setAllowedMethods(
